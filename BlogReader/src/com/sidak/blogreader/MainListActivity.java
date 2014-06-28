bf5856efdec38faa752806573d45b44431d71954
sidak <spssachdeva21695@gmail.com>
2014-06-28 19:19:05 +0530

    use async task for fetching data in 'MainListActivity.java'

--- BlogReader/src/com/sidak/blogreader/MainListActivity.java
+++ BlogReader/src/com/sidak/blogreader/MainListActivity.java
@@ -1,9 +1,12 @@
 package com.sidak.blogreader;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -17,16 +20,38 @@ public class MainListActivity extends ListActivity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
-		try {
-			URL blogFeedUrl= new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count="+ NUMBER_OF_POSTS);
-		
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			Log.e(TAG, "Exception caught:",e);	
-		}
+		GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+		getBlogPostsTask.execute();
 
 		
 		//String message=getString(R.string.no_items);
 		//Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+	}
+	
+	private class GetBlogPostsTask extends AsyncTask<Object, Void, String>{
+
+		@Override
+		protected String doInBackground(Object... params) {
+			int responseCode =-1;	
+			try {
+					URL blogFeedUrl= new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count="+ NUMBER_OF_POSTS);
+					HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+					connection.connect();
+					
+					responseCode =connection.getResponseCode();
+					Log.i(TAG, "code: "+responseCode);
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+					Log.e(TAG, "Exception caught:",e);	
+				} catch (IOException e) {
+					e.printStackTrace();
+					Log.e(TAG, "Exception caught:",e);	
+	
+				}catch (Exception e) {
+					Log.e(TAG, "Exception caught:",e);	
+				}
+				return "code: "+responseCode;
+		}
+		
 	}
 }
